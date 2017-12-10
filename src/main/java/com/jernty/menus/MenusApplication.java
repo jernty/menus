a1668ccd8179661403465c9b8336302f21472a42
Tyler <thaithco@alumni@unca.edu>
2017-12-09 20:42:23 -0500

    init commit of project

---
+++ src/main/java/com/jernty/menus/MenusApplication.java
@@ -0,0 +1,12 @@
+package com.jernty.menus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenusApplication.class, args);
+	}
+}
